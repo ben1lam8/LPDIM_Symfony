11e32e3b76a65d613c254c54a9655e59596fbf06
Benoit LAMIT <benoit.lamit@hotmail.fr>
2018-03-19 15:12:08 +0100

    Added API authenticator

---
+++ src/AppBundle/Security/Authentication/ApiUserPasswordAuthenticator.php
@@ -0,0 +1,106 @@
+<?php
+
+
+namespace AppBundle\Security\Authentication;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class ApiUserPasswordAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * @var EncoderFactoryInterface
+     */
+    private $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException|null $authException
+     * @return Response
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new JsonResponse('Authentication required', Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @param Request $request
+     * @return array|mixed|null
+     */
+    public function getCredentials(Request $request)
+    {
+        if(!$request->headers->has('x-email') || !$request->headers->has('x-password')){
+            return null;
+        }
+
+        return [
+            'email' =>  $request->headers->get("x-email"),
+            'password' =>  $request->headers->get("x-password"),
+        ];
+    }
+
+    /**
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     * @return null|UserInterface
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $userProvider->loadUserByUsername($credentials['email']);
+    }
+
+    /**
+     * @param mixed $credentials
+     * @param UserInterface $user
+     * @return bool
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $encoder = $this->encoderFactory->getEncoder($user);
+
+        return $encoder->isPasswordValid($user->getPassword(), $credentials['password'], null);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return null|JsonResponse|Response
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new JsonResponse('Authentication failed', Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @param Request $request
+     * @param TokenInterface $token
+     * @param string $providerKey
+     * @return null|Response
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function supportsRememberMe()
+    {
+        // API is stateless !
+        return false;
+    }
+}
